b0f8db99d38ae398488e4280ba85bc762eb3cc87
NIck <nick7zmail@gmail.com>
2018-08-11 23:35:55 +0500

    test

--- import.php
+++ import.php
@@ -9,22 +9,14 @@ CREATE TABLE IF NOT EXISTS `sys_date_format` (
   `LINKED_OBJECT` varchar(100) NOT NULL DEFAULT \'\',
   `LINKED_PROPERTY` varchar(100) NOT NULL DEFAULT \'\'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
+');
+SQLExec('
 INSERT INTO `sys_date_format` (`TITLE`, `FORMAT`, `LINKED_OBJECT`, `LINKED_PROPERTY`) VALUES
 (\'Часы\', \'H\', \'ThisComputer\', \'timeNowH\'),
 (\'Минуты\', \'i\', \'ThisComputer\', \'timeNowM\'),
 (\'Время\', \'H:i\', \'ThisComputer\', \'timeNow\'),
 (\'Дата\', \'d1\', \'ThisComputer\', \'dateNow\'),
 (\'День недели\', \'n1\', \'ThisComputer\', \'DayOfWeek\');
-
-ALTER TABLE `sys_date_format`
-  ADD PRIMARY KEY (`ID`);
-
-
-ALTER TABLE `sys_date_format`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
 ');
   
 ?>
